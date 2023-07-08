3cdc6063c80fb824e3678774ff6ac3f1500b5df1
MaheshChauthe <102281195+Mahesh-Chauthe@users.noreply.github.com>
2023-07-08 19:35:18 +0530

    index.php

--- index.php
+++ index.php
@@ -1,13 +1,13 @@
- <!DOCTYPE html>
-<html>
-<body>
-
-<h1>My first PHP page for webhooks </h1>
-
 <?php
-echo "Hello World! FROM OCT BATCH WEBHOOOKKKKKKK testing webhook learning for KUCL batch";
- echo "hello from UNNATI 555555.........";
+$image = ImageCreateTrueColor(270, 150);
+imagesavealpha($image, true);
+$trans_colour = imagecolorallocatealpha($image, 0, 0, 0, 127);
+imagefill($image, 0, 0, $trans_colour);
+$x1 = $y1 = 0 ; 
+$x2 = 270; $y2 = 150; 
+$color = #4a235a; 
+ImageLine($image, $x1, $y1, $x2, $y2, $color);
+header('Content-type: image/png');
+ImagePNG($image);
+ImageDestroy($image);
 ?>
-
-</body>
-</html> 
